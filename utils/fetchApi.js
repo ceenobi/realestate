7263d532c5ace93dfb8368322d31ae47aa0216ff
ceenobi <cobimbachu@gmail.com>
2022-06-30 01:24:22 +0100

    removed env

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async(url) => {
-const {data} = await axios.get((url), {
-headers: {
+const { data } = await axios.get(url, {
+  headers: {
     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-    'x-rapidapi-key': 'd37643d468msh1f1a3e7dcfe4578p1785adjsn74cff320651d'
-  }
+    'x-rapidapi-key': process.env.NEXT_APP_RAPIDAPI_KEY,
+  },
 })
 return data;
 }
